ca8194530bb9e5ff25bf8ba1390ec5345a67c0f0
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2012-10-16 19:52:23 +0200

    added lgpl license and notes

--- src/main/scala/org/beandiff/core/LeafDiff.scala
+++ src/main/scala/org/beandiff/core/LeafDiff.scala
@@ -1,3 +1,22 @@
+/**
+ * Copyright (c) 2012, Tomasz Kaczmarzyk.
+ * 
+ * This file is part of BeanDiff.
+ * 
+ * BeanDiff is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ * 
+ * BeanDiff is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BeanDiff; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.beandiff.core
 
 class LeafDiff(o1: Any, o2: Any) extends Diff(o1, o2, null) {
