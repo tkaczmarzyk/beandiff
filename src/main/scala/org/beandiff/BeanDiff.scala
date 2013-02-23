ec7c662ec9451660c5c0f2b05dfc25ab2f07f529
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-02-23 18:06:31 +0100

    @vargars for BeanDiff methods

--- src/main/scala/org/beandiff/BeanDiff.scala
+++ src/main/scala/org/beandiff/BeanDiff.scala
@@ -19,9 +19,14 @@
  */
 package org.beandiff
 
+import java.io.PrintStream
 import java.io.PrintWriter
+
+import scala.annotation.varargs
+
 import org.beandiff.TypeDefs.JBigDecimal
 import org.beandiff.core.DelegatingDiffEngine
+import org.beandiff.core.DiffEngine
 import org.beandiff.core.EndOnNullStrategy
 import org.beandiff.core.EndOnSimpleTypeStrategy
 import org.beandiff.core.model.Diff
@@ -32,8 +37,6 @@ import org.beandiff.equality.IgnoreCaseStringEqualityInvestigator
 import org.beandiff.equality.StdEqualityInvestigator
 import org.beandiff.support.ClassDictionary
 import org.beandiff.support.ValueTypes
-import java.io.PrintStream
-import org.beandiff.core.DiffEngine
 
 /**
  * A container for syntactic sugar methods
@@ -43,57 +46,61 @@ import org.beandiff.core.DiffEngine
 object BeanDiff {
 
   private type EqInvestigatorBinding = (Class[_], EqualityInvestigator);
-  
+
   final val DefaultDescStrategy = new EndOnNullStrategy(EndOnSimpleTypeStrategy.withLeaf(classOf[JBigDecimal]))
-  
+
   final val DefaultPresenter = new PlainTextDiffPresenter
-  
+
   final val DefaultEqInvestigators: ClassDictionary[EqualityInvestigator] = new ClassDictionary(new StdEqualityInvestigator)
     .withEntry(classOf[JBigDecimal] -> new ComparableEqualityInvestigator)
-  	.withEntries(ValueTypes.all.map((_, new StdEqualityInvestigator)))
-  
+    .withEntries(ValueTypes.all.map((_, new StdEqualityInvestigator)))
+
   final val IgnoreCase = (classOf[String], new IgnoreCaseStringEqualityInvestigator)
 
-    
   def diff(o1: Any, o2: Any): Diff = {
-    diff(o1, o2, List() : _*)
+    diff(o1, o2, List(): _*)
   }
 
+  @varargs
   def diff(o1: Any, o2: Any, modifiers: Any*): Diff = {
     diffEngine(modifiers: _*).calculateDiff(o1, o2)
   }
-  
+
+  @varargs
   def diffEngine(modifiers: Any*): DiffEngine = {
     val eqInvestigators = DefaultEqInvestigators.withEntries(getEqInvestigatorMappings(modifiers.toList))
 
     new DelegatingDiffEngine(eqInvestigators, DefaultDescStrategy)
   }
-  
+
   def mkString(diff: Diff) = DefaultPresenter.present(diff)
-  
+
   def print(diff: Diff): Unit = print(new PrintWriter(System.out), diff)
-  
+
   def print(out: PrintWriter, diff: Diff): Unit = {
     out.println(DefaultPresenter.present(diff))
   }
-  
-  def printDiff(o1: Any, o2: Any): Unit = 
-    printDiff(o1, o2, List() : _*)
-  
+
+  def printDiff(o1: Any, o2: Any): Unit =
+    printDiff(o1, o2, List(): _*)
+
+  @varargs
   def printDiff(o1: Any, o2: Any, modifiers: Any*): Unit =
-    printDiff(new PrintWriter(System.out), o1, o2, modifiers : _*)
-  
+    printDiff(new PrintWriter(System.out), o1, o2, modifiers: _*)
+
   def printDiff(out: PrintStream, o1: Any, o2: Any): Unit =
     printDiff(new PrintWriter(out), o1, o2)
-    
+
   def printDiff(out: PrintWriter, o1: Any, o2: Any): Unit =
-    printDiff(out, o1, o2, List() : _*)
-  
+    printDiff(out, o1, o2, List(): _*)
+
+  @varargs
   def printDiff(out: PrintStream, o1: Any, o2: Any, modifiers: Any*) =
-    print(new PrintWriter(out), diff(o1, o2, modifiers : _*))
-    
+    print(new PrintWriter(out), diff(o1, o2, modifiers: _*))
+
+  @varargs
   def printDiff(out: PrintWriter, o1: Any, o2: Any, modifiers: Any*) =
-    print(out, diff(o1, o2, modifiers : _*))
+    print(out, diff(o1, o2, modifiers: _*))
 
   private def getEqInvestigatorMappings(objects: List[_]) = {
     objects.filter(_.isInstanceOf[EqInvestigatorBinding])
