5b230370f02fb1fdd5b4df31ece10427daa6d617
tkaczmarzyk <tomasz+github@kaczmarzyk.net>
2013-03-09 12:04:09 +0100

    more concise engine labels in test

--- src/test/scala/org/beandiff/TransformationConsistencyTest.scala
+++ src/test/scala/org/beandiff/TransformationConsistencyTest.scala
@@ -32,9 +32,9 @@ import org.beandiff.beans.scala.Child
 @RunWith(classOf[JUnitRunner])
 class TransformationConsistencyTest extends FunSuite with TransformationCheckUtils {
 
-  val enginesWithLabels = List((aDiffEngine, "standard engine"), (aDiffEngine.withEntity[Child]("name"), "Child is entity (name is id)"),
-    (aDiffEngine.withEntity[Parent]("name").withEntity[Child]("name"), "both Parent and Child are entities (name is id)"),
-    (aDiffEngine.withEntity[Parent]("name"), "Parent is entity (name is id)"))
+  val enginesWithLabels = List((aDiffEngine, "standard engine"), (aDiffEngine.withEntity[Child]("name"), "Child is entity"),
+    (aDiffEngine.withEntity[Parent]("name").withEntity[Child]("name"), "both Parent and Child are entities"),
+    (aDiffEngine.withEntity[Parent]("name"), "Parent is entity"))
 
   for ((engine, label) <- enginesWithLabels) {
     test("[" + label + "] transformTarget should transform a collection of Ints into other collection of Ints") {
